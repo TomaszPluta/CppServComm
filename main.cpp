8fd7e6a216d618d65d961af34937dc707e656ff2
TomaszPluta <tomasz.pluta.g2@gmail.com>
2019-07-19 14:11:50 +0200

    refactor start

--- main.cpp
+++ main.cpp
@@ -12,22 +12,20 @@
 #include <ctime> 
 
 
+constexpr int ServPort = 1886;
+constexpr int PoolSize = 4;
+constexpr int ColumnNo = 3;
 
 
-auto t1Lambda = [&] ( ServerSocket new_sock )
+auto WorkerThread = [&] ( ServerSocket new_sock ,  Hqqt::Broker<ServerSocket> &broker)
 {
-
-    Hqqt::Broker<ServerSocket> broker;
     try {
         while (1) {
             std::cout << "child process here:" << std::endl;
             std::string frame;
             new_sock >> frame;
-
             std::cout << "Server got:" << frame <<std::endl;
             std::cout << "from peer addr: " << new_sock.get_cli_addr() <<std::endl;
-            std::string addr;
-            //	  frame = GetFrame();
             new_sock << broker.OnReceivedFrame(frame, new_sock);
         }
     } catch (...) {
@@ -38,31 +36,6 @@ auto t1Lambda = [&] ( ServerSocket new_sock )
 
 
 
-
-void t1 ( ServerSocket new_sock,    Hqqt::Broker<ServerSocket> & broker)
-{
-    try {
-        while (1) {
-              std::cout<<"- - - - - - - - "<<std::endl;
-            std::string frame;
-            new_sock >> frame;
-
-            std::cout << "Server got:" << frame <<std::endl;
-            std::cout << "from peer addr: " << new_sock.get_cli_addr() <<std::endl;
-            std::string addr;
-            std::mutex m;
-            std::unique_lock<std::mutex> mLock(m);
-            new_sock << broker.OnReceivedFrame(frame, new_sock);
-        }
-    } catch (...) {
-        std::cout<<"client closed connection"<<std::endl;
-        exit(0);
-    }
-}
-
-constexpr int ServPort = 1886;
-constexpr int PoolSize = 4;
-constexpr int ColumnNo = 3;
 
 int main ( int argc, char * argv[] )
 {
@@ -78,15 +51,14 @@ for (int i =0; i < users.size(); i++){
     
     Hqqt::Broker<ServerSocket> broker;
     ThreadPool pool(PoolSize);
-
-    std::cout << "Server is running...."<<std::endl;
     ServerSocket server ( ServPort );
+    std::cout << "Server is running...."<<std::endl;
+
     
     while(1) {
       ServerSocket  * cliSocket = new(ServerSocket);
       server.accept (*cliSocket );
-      
-  
+        
      std::chrono::system_clock::time_point timeNow  = std::chrono::system_clock::now();
      std::time_t timeConv =  std::chrono::system_clock::to_time_t(timeNow);
      std::cout << ctime(&timeConv) << std::endl; 
@@ -97,7 +69,7 @@ for (int i =0; i < users.size(); i++){
 
     
         database->Insert("INSERT INTO users(id,status,timeLogin) VALUES (?,?,?)", { "S:"+std::string(cliSocket->get_cli_addr()),"I:100", "S:" + std::string(ctime(&timeConv))});
-        pool.enqueue(t1, std::ref(*cliSocket), std::ref(broker));
+        pool.enqueue(WorkerThread, std::ref(*cliSocket), std::ref(broker));
     }
 
 
